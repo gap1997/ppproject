6512631b3a39f55529d0065144075ddb5187dd83
Gaurav Pawar <gauravpawar8888427099@gmail.com>
2023-05-25 18:36:45 +0530

    Initial commit

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,51 @@
+// import { Injectable } from '@angular/core';
+// import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree
+// }
+//   from '@angular/router';
+// import { Observable } from 'rxjs';
+// import { SellerService } from './services/seller.service';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanActivate {
+//   constructor(private sellerService:SellerService){}
+//   canActivate(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+
+//   if(localStorage.getItem('seller')) {
+//   return true
+// }
+
+//   {
+//     return this.sellerService.isSellerLoggedIn;
+//   }
+
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { SellerService } from './services/seller.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private sellerService: SellerService) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const localStorage = window.localStorage;
+    if (localStorage.getItem('seller')) {
+      return true;
+    } else {
+      return this.sellerService.isSellerLoggedIn.getValue();
+    }
+  }
+}
---
+++ src/app/datatype.ts
@@ -0,0 +1,5 @@
+export interface SignUp{
+    name: "String",
+    Email: "String",
+    Password:"String"
+}
